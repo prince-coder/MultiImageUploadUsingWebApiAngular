59f1a0906814e3c507239d09011cba93b7775b38
prince-coder <prince@promactinfo.com>
2016-06-16 16:40:42 +0530

    Project Files Added

---
+++ AngularWebApiDemo/AngularWebApiDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using AngularWebApiDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularWebApiDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return db.Employee.AsEnumerable();
+        }
+
+    }
+}
---
+++ AngularWebApiDemo/AngularWebApiDemo/Scripts/Employee.js
@@ -0,0 +1,17 @@
+﻿angular.module('MyApp', [])
+    .controller('EmployeeController', function($scope, $http) {
+        debugger;
+        $http.get('http://localhost:61649/api/Employee').success(function (data) {
+            debugger;
+            alert('Success');
+            $scope.Employees = data;
+
+        })
+
+        .error(function () {
+            debugger;
+            $scope.error = "An error occured while loading data";
+
+        });
+
+    });
