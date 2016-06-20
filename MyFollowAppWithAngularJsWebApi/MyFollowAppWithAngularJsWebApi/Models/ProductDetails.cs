5671be5739692a50bf15105dfbcb63c4e215afd0
prince-coder <prince@promactinfo.com>
2016-06-20 18:58:33 +0530

    Project files added

---
+++ MyFollowAppWithAngularJsWebApi/MyFollowAppWithAngularJsWebApi/Models/ProductDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyFollowAppWithAngularJsWebApi.Models
+{
+    public class ProductDetails
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductIntro { get; set; }
+        public string Detail { get; set; }
+        public string Media { get; set; }
+        public int UserId { get; set; }
+        public virtual ProductOwner ProductOwner { get; set; }
+        public ICollection<FollowProduct> FollowProduct { get; set; }
+    }
+}
